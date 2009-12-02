473500f6b10c79dd7aac1ae47e1fc35167df6e04
mathew johnson <mathew@mathew-johnsons-macbook.local>
2009-12-01 22:39:46 -0800

    more s3 photos

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
-has_attached_file :photo
+has_attached_file :photo,
+
+	:storage => :s3,
+    :s3_credentials => "#twapp2/config/s3.yml",
+    :path => ":attachment/:id/:style.:extension",
+    :bucket => 'twapp2'
 
 end
