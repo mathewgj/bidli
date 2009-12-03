cfe415f2bcd292514653373ecfed9504ab3a39e2
mathew johnson <mathew@mathew-johnsons-macbook.local>
2009-12-03 12:59:54 -0800

    make imp links bigger

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ActiveRecord::Base
 has_attached_file :photo,
 
 	:storage => :s3,
-    :s3_credentials => "#twapp2/config/s3.yml",
+    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
     :path => ":attachment/:id/:style.:extension",
     :bucket => 'twapp2'
 
