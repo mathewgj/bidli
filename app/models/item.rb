9765261408209f728389b6596d80362c16ae256c
mathew johnson <mathew@mathew-johnsons-macbook.local>
2010-01-22 09:17:53 -0800

    photo thumbs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
 has_attached_file :photo,
-
+	:styles => {
+		:thumb=> "100x100#",},
+	
 	:storage => :s3,
     :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
     :path => ":attachment/:id/:style.:extension",
